d73876bd0f1f3685c4525555b39ab5454b379f6b
Adhya mittal <mittal.adhya2001@gmail.com>
2022-05-27 22:55:36 +0530

    main

--- face.py
+++ face.py
@@ -1,136 +1,74 @@
-import face_recognition
 import cv2
-from openpyxl import Workbook
-import datetime
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+# from PIL import ImageGrab
+
+path = 'photos'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
 
 
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
 
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
-    
-    
-# Create a woorksheet
-book=Workbook()
-sheet=book.active
-    
-# Load images.
-    
-image_1 = face_recognition.load_image_file("1.jpg")
-image_1_face_encoding = face_recognition.face_encodings(image_1)[0]
-    
-image_2 = face_recognition.load_image_file("2.jpg")
-image_2_face_encoding = face_recognition.face_encodings(image_2)[0]
-    
+def markAttendance( name):
+            with open('Attendance.csv', 'r+') as f:
+               myDataList = f.readlines()
+               nameList = []
+               for line in myDataList:
+                 entry = line.split(',')
+                 nameList.append(entry[0])
+               if name not in nameList:
+                  now = datetime.now()
+                  dtString = now.strftime('%H:%M:%S')   #hour min sec
+                  f.writelines(f'\n{name},{dtString}')  
 
-    
-image_3 = face_recognition.load_image_file("3.jpg")
-image_3_face_encoding = face_recognition.face_encodings(image_3)[0]
-    
 
-    
-    
-# Create arrays of known face encodings and their names
-known_face_encodings = [
-        
-        image_1_face_encoding,
-        image_2_face_encoding,
-        image_3_face_encoding,
-        
-    ]
-known_face_names = [
-        
-        "ADHYA",
-        "modi",
-        "shah rukh khan",
-        
-       
-    ]
-    
-# Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
-    
-# Load present date and time
-now= datetime.datetime.now()
-today=now.day
-month=now.month
-    
-   
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
+cap = cv2.VideoCapture(0)
+
 while True:
- # Grab a single frame of video
-    ret, frame = video_capture.read()
-    
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-    
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
-    
-    # Only process every other frame of video to save time
-    if process_this_frame:
-        # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
-    
-    face_names = []
-    for face_encoding in face_encodings:
-        # See if the face is a match for the known face(s)
-        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-        name = "Unknown"
-    
-         # If a match was found in known_face_encodings, just use the first one.
-        if True in matches:
-            first_match_index = matches.index(True)
-            name = known_face_names[first_match_index]
-    
-        
-    face_names.append(name)
-    
-    process_this_frame = not process_this_frame
-    
-    
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-           # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-           top *= 4
-           right *= 4
-           bottom *= 4
-           left *= 4
-    
-    # Draw a box around the face
-    cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-    
-    # Draw a label with a name below the face
-    cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-    #font = cv2.FONT_HERSHEY_DUPLEX
-    #cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-    
-    if name == "ADHYA":
-            print("adhya")
+    success, img = cap.read()
+# img = captureScreen()
+    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-    elif name == "modi":
-            print("modi")
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
-    elif name == "shah rukh khan":
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+# print(faceDis)
+        matchIndex = np.argmin(faceDis)
 
-    #cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            #cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-            #cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-            print("shah rukh khan")
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            #print(name)
+        #to make the rectangle in the image
+        y1, x2, y2, x1 = faceLoc
+        y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+        markAttendance(name)
 
-    # Display the resulting image
-    cv2.imshow('Video', frame)
-        
-    # Save Woorksheet as present month
-    book.save(str(month)+".xlsx")
-    
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
-    
+    cv2.imshow('Webcam', img)
+    cv2.waitKey(1)
